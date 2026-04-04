8f442c67847f4b72240e3e5d210d31768ef3f019
Peter Jun Park <git@peterjunpark.com>
2026-04-04 17:08:23 -0400

    update configs

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Peter Jun Park";
+export const SITE_TITLE = "peterjunpark";
 export const SITE_DESCRIPTION = "Peter's personal website.";
